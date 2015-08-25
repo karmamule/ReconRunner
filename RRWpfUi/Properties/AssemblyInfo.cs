29586c6c1c5e6ed863a0b372d6814f9c4f45e34a
Eddie Yasi <eddie.yasi@outlook.com>
2015-08-25 15:52:04 -0400

    Figuring out best install method (clickonce or installshield limited?)

--- RRWpfUi/Properties/AssemblyInfo.cs
+++ RRWpfUi/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RRWpfUi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ReconRunner")]
+[assembly: AssemblyDescription("Use to create Excel workbook showing query results or two-query comparison results")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RRWpfUi")]
+[assembly: AssemblyCompany("Acadian Asset Management")]
+[assembly: AssemblyProduct("ReconRunner")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
